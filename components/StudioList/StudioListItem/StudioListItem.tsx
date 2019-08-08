5db643ffd8ead74bca0b5a378ae2b5a9932328bd
IvanGrimes <ivan@grimesco.de>
2019-08-08 11:34:25 +0300

    remove console.log in StudioListItem

--- components/StudioList/StudioListItem/StudioListItem.tsx
+++ components/StudioList/StudioListItem/StudioListItem.tsx
@@ -24,66 +24,62 @@ const _StudioListItem = ({
   types,
   stations,
   roomsCount,
-}: StudioListItemProps) => {
-  console.log(stations);
-
-  return (
-    <Card>
-      <Grid container>
-        <Grid item xs={12}>
-          <Carousel>
-            {photos.map(({ id, ratio }, index) => (
-              <LazyImage
-                key={index}
-                src={`https://via.placeholder.com/${id}`}
-                ratio={floatToFraction(ratio)}
-              />
-            ))}
-          </Carousel>
-        </Grid>
+}: StudioListItemProps) => (
+  <Card>
+    <Grid container>
+      <Grid item xs={12}>
+        <Carousel>
+          {photos.map(({ id, ratio }, index) => (
+            <LazyImage
+              key={index}
+              src={`https://via.placeholder.com/${id}`}
+              ratio={floatToFraction(ratio)}
+            />
+          ))}
+        </Carousel>
       </Grid>
-      <CardContent>
-        <Grid container justify="space-between" alignItems="center" spacing={2}>
-          <Grid item xs={6} container alignItems="center" spacing={4}>
-            <Grid item>
-              <Typography variant="h5" component="h2">
-                {name}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Tooltip title="Количество комнат">
-                <Grid container>
-                  <MeetingRoom /> <Typography>{roomsCount}</Typography>
-                </Grid>
-              </Tooltip>
-            </Grid>
+    </Grid>
+    <CardContent>
+      <Grid container justify="space-between" alignItems="center" spacing={2}>
+        <Grid item xs={6} container alignItems="center" spacing={4}>
+          <Grid item>
+            <Typography variant="h5" component="h2">
+              {name}
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography variant="h5" component="span">
-              {getPriceSegment(priceSegment)}
-            </Typography>
+            <Tooltip title="Количество комнат">
+              <Grid container>
+                <MeetingRoom /> <Typography>{roomsCount}</Typography>
+              </Grid>
+            </Tooltip>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body2" color="textSecondary">
-              {description}
-            </Typography>
-          </Grid>
-          <Grid component="ul" item>
-            {stations.map(({ id, color, name: station }) => (
-              <Station key={id} color={color}>
-                <Typography variant="caption">{station}</Typography>
-              </Station>
-            ))}
-          </Grid>
-          <ChipGrid container item xs={7} justify="flex-end">
-            {types.map(({ id, name: type }) => (
-              <Chip key={id} label={type} size="small" />
-            ))}
-          </ChipGrid>
         </Grid>
-      </CardContent>
-    </Card>
-  );
-};
+        <Grid item>
+          <Typography variant="h5" component="span">
+            {getPriceSegment(priceSegment)}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" color="textSecondary">
+            {description}
+          </Typography>
+        </Grid>
+        <Grid component="ul" item>
+          {stations.map(({ id, color, name: station }) => (
+            <Station key={id} color={color}>
+              <Typography variant="caption">{station}</Typography>
+            </Station>
+          ))}
+        </Grid>
+        <ChipGrid container item xs={7} justify="flex-end">
+          {types.map(({ id, name: type }) => (
+            <Chip key={id} label={type} size="small" />
+          ))}
+        </ChipGrid>
+      </Grid>
+    </CardContent>
+  </Card>
+);
 
 export const StudioListItem = memo(_StudioListItem);
