fd78b1e6888b4b6f9aec08108c78d3c36cb8b6ee
IvanGrimes <ivan@grimesco.de>
2019-09-09 16:51:52 +0300

    remove rewrite ^/api$ in production

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,17 +1,3 @@
 import defaultAxios from 'axios';
 
 export const axios = defaultAxios.create();
-
-axios.interceptors.request.use(config => {
-  if (config.url) {
-    return {
-      ...config,
-      url:
-        process.env.NODE_ENV === 'production'
-          ? config.url.replace('/api', '')
-          : config.url,
-    };
-  }
-
-  return config;
-});
