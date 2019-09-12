a863e1055fba742818dd3aa765328b2a500d76fc
IvanGrimes <ivan@grimesco.de>
2019-09-12 11:37:10 +0300

    move api auth to the proxy lambda

--- lambdas/proxy.ts
+++ lambdas/proxy.ts
@@ -12,8 +12,22 @@ server.use(
   '/api',
   expressHttpProxy('134.209.250.238', {
     proxyReqPathResolver: req => {
-      console.log(req.url, req.url.replace('/api', ''));
       return req.url.replace('/api', '');
+    },
+    proxyReqOptDecorator: proxyReqOpts => {
+      if (!process.env.API_AUTH) {
+        throw new Error('You should define API_AUTH env variable');
+      }
+
+      if (!proxyReqOpts.headers) {
+        // eslint-disable-next-line no-param-reassign
+        proxyReqOpts.headers = {};
+      }
+
+      // eslint-disable-next-line no-param-reassign
+      proxyReqOpts.headers.Authorization = btoa(process.env.API_AUTH);
+
+      return proxyReqOpts;
     },
   })
 );
